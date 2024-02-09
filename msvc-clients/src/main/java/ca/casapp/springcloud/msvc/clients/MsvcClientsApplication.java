fae019010d5b96784da71bae9d1d815fe9e442b0
Dwin Paez <dwinpaez@gmail.com>
2024-02-08 23:18:53 -0500

    first commit

---
+++ msvc-clients/src/main/java/ca/casapp/springcloud/msvc/clients/MsvcClientsApplication.java
@@ -0,0 +1,13 @@
+package ca.casapp.springcloud.msvc.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcClientsApplication.class, args);
+	}
+
+}
---
+++ msvc-clients/src/test/java/ca/casapp/springcloud/msvc/clients/MsvcClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.casapp.springcloud.msvc.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
