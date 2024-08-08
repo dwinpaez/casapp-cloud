ce4d2187bf18c445ad25a6e09674cf7cd896a14b
Dwin Paez <dwinpaez@gmail.com>
2024-08-08 00:06:07 -0400

    add tests

--- msvc-bookings/src/test/java/ca/casapp/springcloud/msvc/bookings/web/controller/BookingControllerTest.java
+++ msvc-bookings/src/test/java/ca/casapp/springcloud/msvc/bookings/web/controller/BookingControllerTest.java
@@ -11,10 +11,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -26,7 +28,7 @@ class BookingControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private ObjectMapper objectMapper= new ObjectMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @MockBean
     private BookingService bookingService;
@@ -42,9 +44,9 @@ class BookingControllerTest {
         mockMvc.perform(get("/booking").accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                    .andExpect(content().string(containsString(objectMapper.writeValueAsString(bookings))));
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(bookings))));
 
-        verify(bookingService,times(1)).findAllBookings();
+        verify(bookingService, times(1)).findAllBookings();
     }
 
     @Test
@@ -58,14 +60,59 @@ class BookingControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString(objectMapper.writeValueAsString(booking))));
 
-        verify(bookingService,times(1)).findBookingById(bookingId);
+        verify(bookingService, times(1)).findBookingById(bookingId);
     }
 
     @Test
-    void testFindBookinsByClient() {
+    void testFindBookingByIdNotFound() throws Exception {
+        Long bookingId = 1L;
+        when(bookingService.findBookingById(bookingId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/booking/{bookingId}", bookingId).accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+        verify(bookingService, times(1)).findBookingById(bookingId);
     }
 
     @Test
-    void testDeleteBookingById() {
+    void testFindBookinsByClient() throws Exception {
+        Long clientId = 1L;
+        BookingDomain booking = mock(BookingDomain.class);
+        when(bookingService.findBookingsByClient(clientId)).thenReturn(List.of(booking));
+
+        mockMvc.perform(get("/booking/my-bookings/{clientId}", clientId).accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(booking))));
+
+        verify(bookingService, times(1)).findBookingsByClient(clientId);
+    }
+
+    @Test
+    void testDeleteBookingById() throws Exception {
+        Long bookingId = 1L;
+        doNothing().when(bookingService).deleteBookingById(bookingId);
+
+        BookingDomain booking = mock(BookingDomain.class);
+        when(bookingService.findBookingById(bookingId)).thenReturn(Optional.of(booking));
+
+        mockMvc.perform(delete("/booking/{bookingId}", bookingId).accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        verify(bookingService, times(1)).deleteBookingById(booking.id());
+    }
+
+    @Test
+    void testDeleteBookingByIdNotFound() throws Exception {
+        Long bookingId = 1L;
+        when(bookingService.findBookingById(bookingId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/booking/{bookingId}", bookingId).accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+        verify(bookingService, times(1)).findBookingById(bookingId);
     }
 }
