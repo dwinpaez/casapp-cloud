796d66e69e40de49555b0fd47fcb5297105087e4
Dwin Paez <dwinpaez@gmail.com>
2024-09-05 23:07:05 -0400

    add spring auth

---
+++ msvc-auth/src/main/java/ca/casapp/springcloud/msvc/auth/msvc_auth/MsvcAuthApplication.java
@@ -0,0 +1,13 @@
+package ca.casapp.springcloud.msvc.auth.msvc_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcAuthApplication.class, args);
+	}
+
+}
---
+++ msvc-auth/src/test/java/ca/casapp/springcloud/msvc/auth/msvc_auth/MsvcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.casapp.springcloud.msvc.auth.msvc_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
